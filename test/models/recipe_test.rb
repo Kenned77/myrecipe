c5102cae63684b37b526ea11c4ce6ea113bdacbc
kenned77 <kenneth.bidstrup@gmail.com>
2017-04-16 09:33:19 +0000

    add password functionality

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
   def setup
-    @chef = Chef.create!(chefname: "Kenneth", email: "kenneth@example.com")
+    @chef = Chef.create!(chefname: "Kenneth", email: "kenneth@example.com",
+                        password: "password", password_confirmation: "password")
     @recipe = @chef.recipes.build(name: "vegitable", description: "great vegitable recipe")
   end
   
